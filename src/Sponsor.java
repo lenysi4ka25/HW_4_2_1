11867cac287c716d3888d8733c84b5ccebe27478
lenysi4ka <lenysi4ka@mail.ru>
2023-01-03 19:08:36 +0300

    Задание № 1 и № 2 - готово Задание № 3 и № 4 - готово Внесла изменения. Добавила исключения в класс транспорт добавила поле "список водителей" создала клас "Sponsor"

---
+++ src/Sponsor.java
@@ -0,0 +1,23 @@
+public class Sponsor {
+
+    private final String nameSponsor;
+
+    private final int amount;
+
+    public Sponsor(String nameSponsor, int amount) {
+        this.nameSponsor = nameSponsor;
+        this.amount = amount;
+    }
+
+    public String getNameSponsor() {
+        return nameSponsor;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void raceSponsor() {
+        System.out.printf("Спонсор \"%s\" пронспонсировал заезд %d%n", nameSponsor, amount);
+    }
+}
